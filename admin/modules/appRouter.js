14700fd959582aa403616fb3e5b8ec7ab4241ab8
b2blosangeles <b2blosangeles@gmail.com>
2020-07-12 19:35:59 -0700

    update

--- admin/modules/appRouter.js
+++ admin/modules/appRouter.js
@@ -3,18 +3,24 @@
 		var fs = require('fs');
 		this.get = function() {
 		    var me = this, p = req.params[0];
-		    var fn = env.root + '/www/' + p;
-		    fs.stat(fn, function(err, stat) {
-			if(err == null) {
-				res.sendFile(fn);
-			} else  {
-				res.render('html/page404.ect');
-			}
-		    });
+		    var fn = env.root + '/www/' + ((!p) ? 'index.html' : p);
+		    me.sendFile(fn);
 		};	
 		this.post = () => {
-            		var me = this;
+            var me = this;
 		};
+
+		this.sendFile = (fn) => {
+			fs.stat(fn, function(err, stat) {
+				if(err == null) {
+					res.sendFile(fn);
+				} else  {
+					res.render('html/page404.ect');
+				}
+		    });
+		}
+
+
 	};
 	if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
 		module.exports = obj;
