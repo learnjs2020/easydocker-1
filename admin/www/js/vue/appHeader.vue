fa6071c9820e95fed90ea8c6bc92089581647a15
b2blosangeles <b2blosangeles@gmail.com>
2020-07-12 23:25:19 -0700

    update

---
+++ admin/www/js/vue/appHeader.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="card shadow m-2">
+        <div class="card-body">
+            <h4>EasyDocker</h4>
+        </div> 
+    </div> 
+</template>
+ 
+<script>
+module.exports = {
+    data: function() {
+        return {
+            list : []
+        }
+    },
+    mounted() {
+    }
+}
+</script>
+ 
+<style>
+</style>
